a8684494047468e907c10ff07ea656a0e411bd13
“LiciaLicari” <licianbsplicari@gmail.com>
2023-10-13 10:13:07 +0200

    fix

--- main.js
+++ main.js
@@ -5,11 +5,31 @@ createApp({
             records: null,
         }
     },
-    mounted() {
-        axios
-            .get('server.php')
+    methods:{
+        fetchData(url){
+            axios
+            .get(url)
             .then(response => {
                 this.records = response.data
             })
+            .catch(err => {
+                this.error = (error.message);
+            }
+                )
+        }
+    }
+    mounted() {
+        axios
+        .request({
+            url: 'server.php',
+            method: 'GET',
+        })
+        .then(response => {
+            console.log(response.data);
+            this.records = response.data;
+        })
+        .catch(error => {
+            console.error(error.message);
+        })
     }
 }).mount('#app')
