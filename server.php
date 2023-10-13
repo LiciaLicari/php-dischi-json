a228e4b9fea8acad04c69f20066c27022de8b0e7
“LiciaLicari” <licianbsplicari@gmail.com>
2023-10-13 10:05:03 +0200

    fix

--- server.php
+++ server.php
@@ -1,9 +1,10 @@
 <?php
 
-$records = json_decode(file_get_contents('records.json'), true);
+$records = file_get_contents('records.json');
+var_dump($records)
 
 # Add a content type header
 header('Content-Type: application/json');
 
-#encode the array and echo it out
+#return the json: encode the array and echo it out
 echo json_encode($records);
